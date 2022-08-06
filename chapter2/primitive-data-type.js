d89e87ed319217a53b4a01dba05e61dac44649c2
Marcio <marcio122@outlook.com.br>
2022-08-06 14:14:24 +0100

    second JS chapter

---
+++ chapter2/primitive-data-type.js
@@ -0,0 +1,5 @@
+// primitive data types
+
+//strings are used to store a text value
+let singleString = 'Marcio';
+let doubleString = "Jeovety";
--- chapter2/variable.js
+++ chapter2/variable.js
@@ -1,2 +1,9 @@
 let firstName = "Marcio";
 x = 04;
+// git pull --rebase origin main
+
+//the first time I create a variable I declare It
+
+let lastName = "Jeovety";
+lastName = "Mukwaxi";
+// a variable consists of 3 parts: 1 declaration type(let,var, const), 2- name of the variable, 3- value
