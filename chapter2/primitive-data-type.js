dbb574a31cc50463d945f8cc2ababed91aa9efcb
Marcio <marcio122@outlook.com.br>
2022-08-06 20:11:22 +0100

    added strings to the folder

--- chapter2/primitive-data-type.js
+++ chapter2/primitive-data-type.js
@@ -3,3 +3,21 @@
 //strings are used to store a text value
 let singleString = 'Marcio';
 let doubleString = "Jeovety";
+
+let language = "JavaScript";
+let message = `Let's learn ${language}!`;
+console.log(message);
+
+//escape characters
+
+let str = "Hello, what's your name? Is it \"Ruth\"?";
+console.log(str);
+
+let str2 = 'Hello, what\'s\ your name? Is it \"Mark\"?';
+console.log(str2);
+
+let str3 = "This is a new \nline";
+console.log(str3);
+
+let str4 = "I have a backslash in the end!\\";
+console.log(str4);
