7d70323ffe791fac456ec5401772ae8a0f083983
Marcio <marcio122@outlook.com.br>
2022-08-06 13:17:53 +0100

    this is my first in this JS book

---
+++ chapter1/ch1_alert.js
@@ -0,0 +1,12 @@
+prompt("How are you?");
+
+let a = 10;
+//I set a to a value of 10
+console.log(a); //this will show up a value of 10
+/*first experience 
+with javascript*/
+
+// random numbers--> this is the command to run random numbers: math.random()
+console.log(Math.random() * 100);
+
+// if I don't wanna have a decimal result, I can choose -->math.floor()
