336dab85ceeb561916652ae1f32b127f624bae01
Marcio <marcio122@outlook.com.br>
2022-08-07 00:38:26 +0100

    added addition and subtraction operators

--- chapter2/operators.js
+++ chapter2/operators.js
@@ -1,1 +1,27 @@
-// addition, subtraction
+// arithmetic operators can be used to perform operation with numbers
+
+//Addition
+let nr1 = 12;
+let nr2 = 14;
+let result = nr1 + nr2;
+console.log(result);
+
+let str1 = "Hello ";
+let str2 = "Marcio";
+let result2 = str1 + str2;
+console.log(result2);
+
+//addition exercise
+let myName = "Marcio";
+let myAge = 20;
+let canIcode = true;
+console.log(`Hello, my name is ${myName}, I am ${myAge} and I can code JavaScript: ${canIcode}`);
+
+//subtraction
+let numb1 = 20;
+let numb2 = 4;
+let strg1 = "Hi";
+let numb3 = 3;
+let result3 = numb1 - numb2;
+let result4 = strg1 - numb3;
+console.log(result3, result4);// 16 NaN
