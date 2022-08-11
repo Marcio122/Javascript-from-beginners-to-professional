3638573468df6ee1570a6c13779f71cc1f39c58e
Marcio <marcio122@outlook.com.br>
2022-08-11 20:42:21 +0100

    Logical Operator commit

---
+++ chapter2/logicalOperators.js
@@ -0,0 +1,20 @@
+// And operator
+let x = 1;
+let y = 2;
+let z = 3;
+
+console.log( x < y && y < z); // true
+console.log(x > y && y < z); // false
+
+// OR operator
+
+console.log( x > y || y < z); // true
+console.log( x > y || y > z); // false
+
+// Not
+let a = true;
+console.log(!a);
+
+let b = 2;
+let c = 3;
+console.log(!(b < c));
