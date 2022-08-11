ca887ab08fa58e754e49c3a0dbd3ecbdf24a0c62
Marcio <marcio122@outlook.com.br>
2022-08-11 21:16:36 +0100

    BMI project

---
+++ chapter2/project1.js
@@ -0,0 +1,10 @@
+// Miles-to-kilometers converter project
+// For reference, 1 mile equals 1.60934 kilometers.
+// The distance of 130 kms is equal to 209.2142 miles
+
+let mile = 1;
+let kilometers = 1.60934;
+
+let kms = 1;
+let finalMile = kms * kilometers;
+console.log(`The distance of ${kms}km is equal to ${finalMile} miles.`)
---
+++ chapter2/project2.js
@@ -0,0 +1,14 @@
+// BMI CALCULATOR
+// 1 inch is equal to 2.54 cm
+// 1 kilo is equal to 2.2046 pounds
+
+let inch = 1;
+let centimenter = 2.54;
+let finalCal = inch * centimenter;
+console.log(`${inch} inch is equal to ${finalCal}cm`);
+
+let pounds = 2.2046;
+let kilo = 2;
+let calculation = pounds * kilo;
+console.log(`${kilo} kilo is equal to ${calculation} kilo`)
+
