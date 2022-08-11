00d6ecee67fe1e781d92033f9582dbe6614ec5b8
Marcio <marcio122@outlook.com.br>
2022-08-11 20:31:35 +0100

    Comparison Operators

---
+++ chapter2/comparisonOperators.js
@@ -0,0 +1,25 @@
+// the outcome of the comparison operators is always a Boolean value
+
+// Equal
+let x = 5;
+let y = "5";
+console.log(x == y);
+console.log(x === y);
+
+// Not equal
+
+let n = 5;
+let n2 = "5";
+console.log(n != n2);
+console.log(n !== n2);
+
+//greator operator
+
+//greater than
+
+let number = 5;
+let number2 = 6;
+console.log(number2 > number);
+console.log(number > number2);
+console.log(number === number);
+console.log(number >= number2);
